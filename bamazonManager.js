03f38ba3f10d5c57b7a35bd2ff9fc5732e967cf3
Matt <777mth@gmail.com>
2018-02-13 18:32:48 -0500

    add manager functionality

--- bamazonManager.js
+++ bamazonManager.js
@@ -10,6 +10,7 @@ var con = mysql.createConnection({
 });
 
 function managerFunction() {
+  console.log("");
   inquirer.prompt([
     {
       type: "list",
@@ -28,16 +29,18 @@ function managerFunction() {
       case "View Products for Sale" : 
         con.query("SELECT * FROM products", function (err, result) {
           if (err) throw err;
+          console.log("");
           result.forEach(function(item) {
             console.log(`${item.id} | ${item.product_name}   $${item.price}, ${item.stock_quantity}`);
           })
+          managerFunction();
         });
-        managerFunction();
         break;
       case "View Low Inventory" :
         con.query("SELECT * FROM products", function (err, result) {
           if (err) throw err;
           var lowInv = false;
+          console.log("");
           result.forEach(function(item) {
             if (item.stock_quantity < 5) {
               console.log(`${item.id} | ${item.product_name}   $${item.price}, ${item.stock_quantity}`);
@@ -46,16 +49,63 @@ function managerFunction() {
           })
           if (!lowInv) {
             console.log("No low inventory to report.");
+            console.log("");
           }
+          managerFunction();
         });
-        managerFunction();
         break;  
       case "Add to Inventory" :
-        
-        
-      case "Exit" : con.end(); break;
+        inquirer.prompt([
+          {
+            name: "itemNumber",
+            message: "Enter item number: "
+          },
+          {
+            name: "quantity",
+            message: "How many added to stock?"
+          }
+        ]).then(function(answer) {
+          var itemStocked = { id: answer.itemNumber };
+          con.query(`SELECT id, product_name, stock_quantity FROM products`, function(err, result) {
+            if (err) throw err;
+            itemStocked.name = result[answer.itemNumber-1].product_name;
+            itemStocked.quantity = parseFloat(result[answer.itemNumber-1].stock_quantity) + parseFloat(answer.quantity);
+            con.query(`UPDATE products SET stock_quantity = ${itemStocked.quantity} WHERE id = ${answer.itemNumber};`, function(err, result) {
+              if (err) throw err;
+              console.log(`\n${itemStocked.name}s added. New unit count: ${itemStocked.quantity}`);
+              managerFunction();
+            })
+          })
+        })
+        break;
+      case "Add New Product" :
+        inquirer.prompt([
+          {
+            name: "productName",
+            message: "What is the product name?"
+          },
+          {
+            name: "departmentName",
+            message: "What department?"
+          },
+          {
+            name: "price",
+            message: "What is the unit price?"
+          },
+          {
+            name: "quantity",
+            message: "How many being stocked?"
+          }
+        ]).then(function(answer) {
+          con.query(`INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('${answer.productName}', '${answer.departmentName}', '${answer.price}', '${answer.quantity}');`, function (err, result) {
+            if (err) throw err;
+            console.log("\nNew item added!");
+            managerFunction();
+          });
+        })
+        break;
+      case "Exit" : console.log(""); con.end(); break;
     }
-    con.end();
   })
 }
 
